50fa40696c1cf8bab95687190967743ff06d1989
ahmed-bhh <ahmedbelhajhassen22@gmail.com>
2024-12-15 20:08:33 +0100

    push

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace StaticSiteExample.Models;
-
-public class ErrorViewModel
+namespace WebApplicationstatic.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
